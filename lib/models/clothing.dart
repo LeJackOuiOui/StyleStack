edfef6d3b969de8ae52b540d3f52dc85c22fcaad
Diego Castillo <diegocastillo2631@gmail.com>
2026-04-10 13:30:00 -0500

    feat: add toMap/fromMap

--- lib/models/clothing.dart
+++ lib/models/clothing.dart
@@ -12,4 +12,20 @@ class Clothing {
     required this.imagePath,
     this.isFromApi = false,
   }); 
+
+  Map<String, dynamic> toMap() => {
+      'id': id,
+      'name': name,
+      'category': category,
+      'imagePath': imagePath,
+      'isFromApi': isFromApi,
+    };
+
+  factory Clothing.fromMap(Map<String, dynamic> map) => Clothing(
+      id: map['id'],
+      name: map['name'],
+      category: map['category'],
+      imagePath: map['imagePath'],
+      isFromApi: map['isFromApi'] ?? false,
+    );
 }
